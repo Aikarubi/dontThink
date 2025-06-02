44e82c7e0bad7fc7641c8233c88e11343b83f7d5
Aikarubi <nerupe@floridauniversitaria.es>
2025-06-02 11:11:04 +0200

    añadiendo navbar y nuevas views

---
+++ dont-think/src/app/views/decide/decide.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-decide',
+  standalone: true,
+  imports: [],
+  templateUrl: './decide.component.html',
+  styleUrl: './decide.component.css'
+})
+export class DecideComponent {
+
+}
