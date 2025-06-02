5993e9e9c775108d9dbad2891be0c60d7b25cde4
Aikarubi <nerupe@floridauniversitaria.es>
2025-06-02 11:40:00 +0200

    añadiendo estructura y estilos a Zen View

--- dont-think/src/app/views/zen/zen.component.ts
+++ dont-think/src/app/views/zen/zen.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-zen',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './zen.component.html',
   styleUrl: './zen.component.css'
 })
 export class ZenComponent {
 
+  public frase = 'Su frase aparecerá aquí';
 }
