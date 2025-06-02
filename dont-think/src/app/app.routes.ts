e998b46266511ecbf22ad8db16eefebab1e40420
Aikarubi <nerupe@floridauniversitaria.es>
2025-06-02 11:16:58 +0200

    añadiendo rutas y html y css a Decide View

--- dont-think/src/app/app.routes.ts
+++ dont-think/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { DecideComponent } from './views/decide/decide.component';
+import { ZenComponent } from './views/zen/zen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '', redirectTo: 'decide', pathMatch: 'full'
+    },
+    {
+        path: 'decide', component: DecideComponent
+    },
+    {
+        path: 'zen', component: ZenComponent
+    }
+];
--- dont-think/src/app/views/decide/decide.component.ts
+++ dont-think/src/app/views/decide/decide.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-decide',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './decide.component.html',
   styleUrl: './decide.component.css'
 })
