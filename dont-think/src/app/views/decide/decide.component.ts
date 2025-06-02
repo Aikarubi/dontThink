b6732a3d736274a2d7a1131cbbbc1cb3078bb739
Aikarubi <nerupe@floridauniversitaria.es>
2025-06-02 11:21:49 +0200

    cambiando el footer de lugar

--- dont-think/src/app/views/decide/decide.component.ts
+++ dont-think/src/app/views/decide/decide.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-decide',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './decide.component.html',
   styleUrl: './decide.component.css'
 })
